9fa8f50f04fb0c1cef882f285abbbff223115e18
hakim sedoud <hakim.sedou@gmail.com>
2023-09-13 19:42:43 +0200

    first maj

--- pages/index.js
+++ pages/index.js
@@ -1,131 +1,103 @@
+import React, { useState } from 'react';
+import data from '../public/list.json';
+import styles from './index.module.css';
+import { faStar as fullStar } from '@fortawesome/free-solid-svg-icons';
+import { faStar as emptyStar } from '@fortawesome/free-regular-svg-icons';
+import { faStarHalfAlt } from '@fortawesome/free-solid-svg-icons';
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
 import Head from 'next/head';
-import styles from '../styles/Home.module.css';
 
-export default function Home() {
-  return (
-    <div className={styles.container}>
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+function App() {
+    const [searchTerm, setSearchTerm] = useState('');
 
-      <main>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
+    const renderStars = (note) => {
+        const totalStars = 5;
+        const stars = [];
+        const fullStarsCount = Math.floor(note);
+        const halfStarExists = note - fullStarsCount !== 0;
 
-        <p className={styles.description}>
-          Get started by editing <code>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
-
-      <footer>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel" className={styles.logo} />
-        </a>
-      </footer>
-
-      <style jsx>{`
-        main {
-          padding: 5rem 0;
-          flex: 1;
-          display: flex;
-          flex-direction: column;
-          justify-content: center;
-          align-items: center;
+        for (let i = 1; i <= totalStars; i++) {
+            if (i <= fullStarsCount) {
+                stars.push(
+                    <FontAwesomeIcon
+                        key={i}
+                        icon={fullStar}
+                        className={styles.starIcon}
+                        color="gold"
+                    />
+                );
+            } else if (halfStarExists && i === fullStarsCount + 1) {
+                stars.push(
+                    <FontAwesomeIcon
+                        key={i}
+                        icon={faStarHalfAlt}
+                        className={styles.starIcon}
+                        color="gold"
+                    />
+                );
+            } else {
+                stars.push(
+                    <FontAwesomeIcon
+                        key={i}
+                        icon={emptyStar}
+                        className={styles.starIcon}
+                        color="grey"
+                    />
+                );
+            }
         }
-        footer {
-          width: 100%;
-          height: 100px;
-          border-top: 1px solid #eaeaea;
-          display: flex;
-          justify-content: center;
-          align-items: center;
-        }
-        footer img {
-          margin-left: 0.5rem;
-        }
-        footer a {
-          display: flex;
-          justify-content: center;
-          align-items: center;
-          text-decoration: none;
-          color: inherit;
-        }
-        code {
-          background: #fafafa;
-          border-radius: 5px;
-          padding: 0.75rem;
-          font-size: 1.1rem;
-          font-family:
-            Menlo,
-            Monaco,
-            Lucida Console,
-            Liberation Mono,
-            DejaVu Sans Mono,
-            Bitstream Vera Sans Mono,
-            Courier New,
-            monospace;
-        }
-      `}</style>
 
-      <style jsx global>{`
-        html,
-        body {
-          padding: 0;
-          margin: 0;
-          font-family:
-            -apple-system,
-            BlinkMacSystemFont,
-            Segoe UI,
-            Roboto,
-            Oxygen,
-            Ubuntu,
-            Cantarell,
-            Fira Sans,
-            Droid Sans,
-            Helvetica Neue,
-            sans-serif;
-        }
-        * {
-          box-sizing: border-box;
-        }
-      `}</style>
-    </div>
-  );
+        return (
+            <figure className={styles.starContainer}>
+                <div className={styles.stars}>
+                    {stars}
+                </div>
+                <figcaption>Note de l'infirmière: {note} sur 5</figcaption>
+            </figure>
+        );
+    };
+
+    return (
+        <main className={styles.App}>
+            <Head>
+                <title>Infirmières à Tremblay-en-France - Annuaire</title>
+                <meta name="description" content="Annuaire des infirmières à Tremblay-en-France. Trouvez et contactez les meilleures professionnelles de la santé." />
+                <link rel="canonical" href="http://votresite.com/" />
+            </Head>
+            <h1 className={styles.Header}>Annuaire des Infirmières à Tremblay-en-France</h1>
+            <input
+                type="text"
+                placeholder="Cherchez par nom..."
+                aria-label="Chercher une infirmière par nom"
+                onChange={(event) => {
+                    setSearchTerm(event.target.value);
+                }}
+                className={styles.Input}
+            />
+            <div className={styles.DirectoryList}>
+                {data
+                    .filter((val) => {
+                        if (searchTerm === "") {
+                            return val;
+                        } else if (val.name.toLowerCase().includes(searchTerm.toLowerCase())) {
+                            return val;
+                        }
+                        return null;
+                    })
+                    .sort((a, b) => b.note - a.note)
+                    .map((infirmiere) => (
+                        <a href={`tel:${infirmiere.phone}`} key={infirmiere.name} className={`${styles.Entry} ${styles.PhoneLink}`}>
+                            <h2 className={styles.Name}>{infirmiere.name}</h2>
+                            <p className={styles.Phone}>Téléphone: {infirmiere.phone}</p>
+                            <p className={styles.Address}>Adresse: {infirmiere.address}</p>
+                            {infirmiere.note && <div className={styles.Note}>{renderStars(infirmiere.note)}</div>}
+                        </a>
+                    ))}
+            </div>
+        </main>
+    );
 }
+
+export default App;
